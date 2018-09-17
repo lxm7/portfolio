39d26f6c22c6dc7a827361018d7ac49e4742576a
lxm7 <lxmrtn7@gmail.com>
2018-09-17 19:53:58 +0100

    Local to live blog test

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -2,8 +2,8 @@ import CMS from 'netlify-cms'
 
 import AboutPagePreview from './preview-templates/AboutPagePreview'
 import BlogPostPreview from './preview-templates/BlogPostPreview'
-import ProductPagePreview from './preview-templates/ProductPagePreview'
+// import ProductPagePreview from './preview-templates/ProductPagePreview'
 
 CMS.registerPreviewTemplate('about', AboutPagePreview)
-CMS.registerPreviewTemplate('products', ProductPagePreview)
+// CMS.registerPreviewTemplate('products', ProductPagePreview)
 CMS.registerPreviewTemplate('blog', BlogPostPreview)
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -17,9 +17,6 @@ const Navbar = () => (
         <Link className="navbar-item" to="/about">
           About
         </Link>
-        <Link className="navbar-item" to="/products">
-          Products
-        </Link>
       </div>
       <div className="navbar-end">
         <a
