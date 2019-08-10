4f41be95a7f86d7a370c3838e6e0a15caeda93ba
Alex Moreton <lxmrtn7@gmail.com>
2019-08-10 10:44:16 +0100

    Improve layout with consistent blog image sizes and use flexbox for grid layout

--- public/styles-bf37ef8c4eda0fbb1832.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{188:function(n,w,o){}}]);
-//# sourceMappingURL=styles-bf37ef8c4eda0fbb1832.js.map
--- src/pages/index.js
+++ src/pages/index.js
@@ -7,22 +7,32 @@ export default class IndexPage extends React.Component {
   render() {
     const { data } = this.props
     const { edges: posts } = data.allMarkdownRemark
+    const rows = [...Array( Math.ceil(posts.length / 4) )];
+    const postRows = rows.map( (_, idx) => posts.slice(idx * 4, idx * 4 + 4) );
 
     return (
       <Layout>
-        <section className="section">
-          <div className="container">
-            {posts.map(({ node: post }) => (
-                <div className="content" key={post.id}>
-                    <h4>{post.frontmatter.title}</h4>
-                    <a className="image_wrap has-text-primary" href={post.frontmatter.url}>
+        {
+          <section className="section">
+            {postRows.map((row, idx) => (
+              <div className="Grid Grid--gutters Grid--full large-Grid--fit" key={idx}>    
+                {row.map(({node: post}) =>
+                  <article key={post.id} className="Grid-cell content">
+                    <div style={{width: '100%'}}>
+                      <h3>{post.frontmatter.title}</h3>
+
+                      <a className="image_wrap has-text-primary" href={post.frontmatter.url}>
                         <img src={post.frontmatter.image} alt='' />
-                    </a>
-                    <p>{post.frontmatter.tags.join(' | ')}</p>
-                </div>
+                      </a>
+
+                      <p>{post.frontmatter.tags.join(' | ')}</p>
+                    </div>
+                  </article>  
+                )}
+              </div>
             ))}
-          </div>
-        </section>
+          </section>
+        }
       </Layout>
     )
   }
